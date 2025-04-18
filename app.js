8e37a71634ae62e5b7d1bc39e053c56a69f92ac1
Mago75Teo <magaldi.matteo@gmail.com>
2025-04-18 12:34:45 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+function extractPages() {
+  alert("Funzione Estrai Pagine - da implementare");
+}
+function exportImage() {
+  alert("Funzione Esporta come PNG - da implementare");
+}
+function compressPDF() {
+  alert("Funzione Comprimi PDF - da implementare");
+}
+function rotatePages() {
+  alert("Funzione Ruota Pagine - da implementare");
+}
---
+++ service-worker.js
@@ -0,0 +1,7 @@
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installato');
+});
+self.addEventListener('fetch', function(event) {
+  // Gestione cache o fetch personalizzati
+});
